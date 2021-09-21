c1721ac7284728bc739bda2a1eb4ae1f596422bb
Anderson <andersoncleyson@protonmail.com>
2021-09-20 21:57:39 -0300

    login

--- api/src/seguranca/seguranca.go
+++ api/src/seguranca/seguranca.go
@@ -1,15 +1,15 @@
 package seguranca
 
 import (
-  "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Hash recebe uma string e coloca um hash
 func Hash(senha string) ([]byte, error) {
-  return bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
+	return bcrypt.GenerateFromPassword([]byte(senha), bcrypt.DefaultCost)
 }
 
 //VerificarSenha compara uma senha e um hash e retorna se elas são iguais
-func VerificarSenha(senhaString, senhaComHash string) error {
-  return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senhaString))
+func VerificarSenha(senhaComHash, senhaString string) error {
+	return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senhaString))
 }

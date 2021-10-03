65ea4a710f7b2bf2e23b70cb159f7bff82de202a
Anderson <andersoncleyson@protonmail.com>
2021-10-03 11:36:38 -0300

    unfollow

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -171,3 +171,18 @@ func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+// PararDeSeguir faz um usuário parar de seguir outro usuario
+func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"delete from seguidores where usuario_id = ? and seguidor_id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+	return nil
+}

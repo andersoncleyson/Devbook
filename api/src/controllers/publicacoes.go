f4fb889b758a110c9c0d00f98e5a3d7369b2720e
Anderson <andersoncleyson@protonmail.com>
2021-11-18 22:21:08 -0300

    Rotas de publicações

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao adciona uma nova publicacao no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request){
+
+}
+
+// BuscarPublicacao tras as publicaçoes que apareceriam no feed do usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request){
+
+}
+
+// BuscarPublicacao traz uma única publicação
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request){
+
+}
+
+// AtualizarPublicacao altera uma publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request){
+
+}
+
+// DeletarPublicacao exclui uma publicação
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request){
+
+}
